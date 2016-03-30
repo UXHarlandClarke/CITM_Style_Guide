8417ae174b1bcc55d2d2df1b8b52b6c9d2adcce1
John Forsythe <John.Forsythe@harlandclarke.com>
2016-03-30 15:01:57 -0500

    Create a list build of city.css, clean it up for prod.

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,28 @@
 'use strict';
  
 var gulp = require('gulp');
-var sass = require('gulp-sass');
- 
+var gp_sass = require('gulp-sass');
+var gp_uglifycss = require('gulp-uglifycss');
+var gp_cleancss = require('gulp-clean-css');
+
 gulp.task('sass', function () {
   return gulp.src('./sass/*.scss')
-    .pipe(sass().on('error', sass.logError))
+    .pipe(gp_sass().on('error', gp_sass.logError))
     .pipe(gulp.dest('./css'));
 });
  
 gulp.task('sass:watch', function () {
   gulp.watch('./sass/*.scss', ['sass']);
 });
+
+gulp.task('dist', function(){
+	gulp.src('./css/*.css')
+    .pipe(gp_uglifycss({
+      "maxLineLen": 80,
+      "uglyComments": true
+     }))
+    .pipe(gp_cleancss({compatibility: 'ie8'}))
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('default', ['sass', 'dist'], function(){});
