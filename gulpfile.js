9bf756061117ee30432200cd0a9333221ecd35bb
John Forsythe <John.Forsythe@harlandclarke.com>
2016-04-04 09:16:00 -0500

    Work from over the weekend, adding JS libs including sprockets-esque include support. Adjusted index.html.

--- gulpfile.js
+++ gulpfile.js
@@ -3,32 +3,54 @@
 var gulp = require('gulp');
 var gp_sass = require('gulp-sass');
 var gp_uglifycss = require('gulp-uglifycss');
+var gp_uglifyjs = require('gulp-uglify');
 var gp_cleancss = require('gulp-clean-css');
+var gp_autoprefixer = require('gulp-autoprefixer');
+var gp_include = require("gulp-include");
+var gp_rename = require("gulp-rename");
+var gp_stripdebug = require("gulp-strip-debug");
 var gp_browserSync = require('browser-sync').create();
 var gp_runsequence = require('run-sequence');
 
 var APP_NAME = require('./package.json').filename;
 
+
+gulp.task('default', ['build-dev', 'app'], function(){});
+
+
+/* build sass/js */
 gulp.task('build-dev', function(done) {
     gp_runsequence(
         'sass',
         'dist-css',
+        'js',
+        'dist-js',
         done);
 });
 
-gulp.task('updatebrowsers', function() {
-	gp_browserSync.reload
-});
-gulp.task('sass:watch', function () {
-  gulp.watch('./utk/sass/' + APP_NAME + '.scss', ['sass']);
-});
 
+gulp.task('app', ['sass'], function() {
+    gp_browserSync.init({
+		server: {
+		    baseDir: "app",
+		    routes: {
+		        "/dist": "dist"
+		    }
+		}
+    });
 
+    /* utk updated */
+    gulp.watch(['./utk/sass/*.scss','./utk/js/*.js'], ['build-dev']);
+
+    /* app updated */
+    gulp.watch('app/*.*').on('change', gp_browserSync.reload);
+});
 
 
 gulp.task('sass', function () {
   return gulp.src('./utk/sass/' + APP_NAME + '.scss')
     .pipe(gp_sass().on('error', gp_sass.logError))
+    .pipe(gp_autoprefixer())
     .pipe(gulp.dest('./utk/css'));
 });
  
@@ -45,21 +67,33 @@ gulp.task('dist-css', function(){
 });
 
 
-gulp.task('app', ['sass'], function() {
-    gp_browserSync.init({
-		server: {
-		    baseDir: "app",
-		    routes: {
-		        "/dist": "dist"
-		    }
-		}
-    });
-
-    gulp.watch(['./utk/sass/*.scss','./utk/js/*.js'], ['build-dev']);
-    gulp.watch('app/*.html').on('change', gp_browserSync.reload);
+gulp.task('js', function () {
+  return gulp.src('./utk/js/' + APP_NAME + '.js')
+    .pipe(gp_include())
+    .pipe(gp_uglifyjs())
+    .pipe(gp_rename('./utk/js.min/' + APP_NAME + '.min.js'))
+      .on('error', console.log)
+	.pipe(gulp.dest('./'));
 });
 
 
+gulp.task('dist-js', function(){
+	gulp.src('./utk/js.min/' + APP_NAME + '.min.js')
+    .pipe(gp_stripdebug())
+    .pipe(gulp.dest('./dist/js/'))
+    .pipe(gp_browserSync.stream());
+}); 
 
 
-gulp.task('default', ['build-dev', 'app'], function(){});
+
+/* phase out */
+
+gulp.task('updatebrowsers', function() {
+	gp_browserSync.reload
+});
+
+gulp.task('sass:watch', function () {
+  gulp.watch('./utk/sass/' + APP_NAME + '.scss', ['sass']);
+});
+
+
