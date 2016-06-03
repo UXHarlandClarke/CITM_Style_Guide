2355adf5b9756cef5a9a5b51e2a146a99428c9ef
John Forsythe <John.Forsythe@harlandclarke.com>
2016-06-03 16:12:27 -0500

    Update 2016-06-03T21:12:24.877Z

---
+++ utk/js/citm.min.js
@@ -0,0 +1,6 @@
+// sprockets style includes
+
+//=include ../../bower_components/jquery/dist/jquery.js
+//=include ../../bower_components/bootstrap-sass/assets/javascripts/bootstrap.js
+
+//=include ./debug.js
