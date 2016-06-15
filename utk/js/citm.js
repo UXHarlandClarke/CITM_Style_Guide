9658bb97f9894816dc9a824b28981fb6906fda39
Justin Dietert <justin.dietert@harlandclarke.com>
2016-06-14 21:22:38 -0500

    add touch swipe to .carousel

--- utk/js/citm.js
+++ utk/js/citm.js
@@ -5,3 +5,16 @@
 //=include ../../bower_components/bootstrap-sass/assets/javascripts/bootstrap.js
 
 //=include ./debug.js
+
+
+// Add swipe functionality to Bootstrap carousels
+$(document).ready(function() {
+  
+    $(".carousel").swiperight(function() {
+        $(this).carousel('prev');
+    });
+    $(".carousel").swipeleft(function() {  
+        $(this).carousel('next');
+    });
+ 
+}); /* END document ready */
