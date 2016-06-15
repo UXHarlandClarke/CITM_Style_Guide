dac5ae3cc4aadf92b7a9f345a31094feef762336
Justin Dietert <justin.dietert@harlandclarke.com>
2016-06-14 21:16:00 -0500

    add jquery touch events include to citm.js

--- utk/js/citm.js
+++ utk/js/citm.js
@@ -1,6 +1,7 @@
 // sprockets style includes
 
 //=include ../../bower_components/jquery/dist/jquery.js
+//=include ../../bower_components/jquery-touch-events/src/jquery.mobile-events.js
 //=include ../../bower_components/bootstrap-sass/assets/javascripts/bootstrap.js
 
 //=include ./debug.js
