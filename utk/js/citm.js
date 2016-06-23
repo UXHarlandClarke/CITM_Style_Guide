1c75f391b5148473a52729f20a87832f7bf15dab
Justin Dietert <justin.dietert@harlandclarke.com>
2016-06-22 21:35:58 -0500

    testing 'touchstart' for activating nav dropdowns on mobile

--- utk/js/citm.js
+++ utk/js/citm.js
@@ -18,12 +18,19 @@ $(document).ready(function() {
     });
     
 
-    // Disable click on hover dropdowns
+    // Click and Tap functionality for dropdowns
     // ---------------------------------------------------
     
-    // UTK top menu
+    // UTK top menu 
+    // (disable click so menu stays open if clicking while hovering)
     $('#utk-menu .dropdown-toggle').click(function() { 
         return false; 
+    });
+    
+    // UTK top menu 
+    // (allow menu to be opened on mobile 'touchstart')
+    $('#utk-menu .dropdown').on('touchstart', function() { 
+        $(this).toggleClass('open');
     });
  
 }); /* END document ready */
