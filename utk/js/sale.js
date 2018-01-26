9f424011a5b1162c5d69563d3fd9b6f09f2fcb45
Preston Bezant <Preston.Bezant@harlandclarke.com>
2018-01-26 09:04:27 -0600

    changed load time to when window is ready and added a price--sale

--- utk/js/sale.js
+++ utk/js/sale.js
@@ -1,5 +1,5 @@
-//jQuery(function(){
-$(document).ready(function() {
+//$(document).ready(function() {
+$( window ).on( "load", function(){
 	if (window.location.hostname == "localhost") {
         var s = {}
         s.pageType = "index";
@@ -14,14 +14,22 @@ $(document).ready(function() {
         } else {
             console.log("FTO cookie already set")
             console.log(Cookies.get('FTO'));
+            $('.gallery-item-home .price-group').css({'line-height':'0.75em','padding-bottom':'0.75em'});
         	$('.gallery-item-title').css({'clear':'both'});
             $('.price-group .price').removeClass('price--lg').removeClass('price--sm').addClass('price--xs');
-            $('.price-group').css({'text-decoration':'line-through','padding-top':'1.4em'});
+            $('.price-group .price').css({'opacity':'0.5'});
 
-            $('.price-group').before(
-            "<p class='price-group sale' style='float:left'>Now <span class='price sale price--lg'><sup class='price__currency'>$</sup>"+
+            // $('.price-group .price').css({'opacity':'0.5','padding-top':'1.4em'});
+            // $('.price-group').before(
+            // "<p class='price-group sale' style='float:left; line-height:0.5; padding-top:1.4em;'>Now <span class='price sale price--lg'><sup class='price__currency'>$</sup>"+
+            // "<span class='price__dollar'>"+Cookies.get('FTO').split('.')[0]+"</span><span class='price__mark'>.</span>"+
+            // "<sup class='price__sup'>"+Cookies.get('FTO').split('.')[1]+"</sup></span></p>"
+            // );
+
+            $('.price-group').prepend(
+            "Now <span class='price sale price--lg'><sup class='price__currency'>$</sup>"+
             "<span class='price__dollar'>"+Cookies.get('FTO').split('.')[0]+"</span><span class='price__mark'>.</span>"+
-            "<sup class='price__sup'>"+Cookies.get('FTO').split('.')[1]+"</sup></span></p>"
+            "<sup class='price__sup'>"+Cookies.get('FTO').split('.')[1]+"</sup></span>"
             );
             $('.gallery-item').prepend("<div class='flag flag-sale'>Sale</div>");
         }
@@ -29,22 +37,25 @@ $(document).ready(function() {
         if((typeof Cookies.get("FTO") !== "undefined") && (Cookies.get('FTO').Seen =='False')){
         	if(s.pageName=="product"){
         		var price = Cookies.get('FTO');
-        		$('[id*=ctl00_spnPrice]').css({'text-decoration':'line-through'}).before(
-	            "<span class='price-group sale' style='position: absolute; right: 5em;'><span class='price sale price--lg'><sup class='price__currency'>$</sup>"+
+        		//$('[id*=ctl00_spnPrice]').css({'text-decoration':'line-through'});
+        		$('[id*=ct100_spnPrice]').before(
+	            "<span class='price-group sale' style='position: absolute; right: 5em;'><span class='price price--lg'><sup class='price__currency'>$</sup>"+
 	            "<span class='price__dollar'>"+price.toString().split('.')[0]+"</span><span class='price__mark'>.</span>"+
 	            "<sup class='price__sup'>"+price.toString().split('.')[1]+"</sup></span></span>"
 	            );
 
 	            price = Cookies.get('FTO')*2;
-	            $('[id*=ctl01_spnPrice]').css({'text-decoration':'line-through'}).before(
-	            "<span class='price-group sale' style='position: absolute; right: 5em;'><span class='price sale price--lg'><sup class='price__currency'>$</sup>"+
+	            //$('[id*=ctl01_spnPrice]').css({'text-decoration':'line-through'});
+	            $('[id*=ct101_spnPrice]').before(
+	            "<span class='price-group sale' style='position: absolute; right: 5em;'><span class='price price--lg'><sup class='price__currency'>$</sup>"+
 	            "<span class='price__dollar'>"+price.toString().split('.')[0]+"</span><span class='price__mark'>.</span>"+
 	            "<sup class='price__sup'>"+price.toString().split('.')[1]+"</sup></span></span>"
 	            );
 
 	            price = Cookies.get('FTO')*3;
-	            $('[id*=ctl02_spnPrice]').css({'text-decoration':'line-through'}).before(
-	            "<span class='price-group sale' style='position: absolute; right: 5em;'><span class='price sale price--lg'><sup class='price__currency'>$</sup>"+
+	            //$('[id*=ctl02_spnPrice]').css({'text-decoration':'line-through'});
+	            $('[id*=ct102_spnPrice]').before(
+	            "<span class='price-group sale' style='position: absolute; right: 5em;'><span class='price price--lg'><sup class='price__currency'>$</sup>"+
 	            "<span class='price__dollar'>"+price.toString().split('.')[0]+"</span><span class='price__mark'>.</span>"+
 	            "<sup class='price__sup'>"+price.toString().split('.')[1]+"</sup></span></span>"
 	            );
